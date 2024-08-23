bfd06c6711990c2e7662b4e5d1643a12aa57a6d2
Caio <ocaiomoises@gmail.com>
2024-08-22 23:09:22 -0300

    Update - recebe dados

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const handleSubmit = (event) => {
     event.preventDefault();
 
+    const name = document.querySelector("input[name=name]");
+    const email = document.querySelector("input[name=email]");
+
     fetch("https://api.sheetmonkey.io/form/2bseHC7mKXKjPqNj2Z3czm", {
         method: "post",
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-            name: "Carlos",
-            email: "carlos@gmail.com"
-        })
+        body: JSON.stringify({name, email})
     });
 }
 
